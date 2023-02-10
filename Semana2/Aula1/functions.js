0ee97f9328a5b3d437819d84f9f8136a00897585
Edilene <seu@email.comedinunnes15@gmail.com>
2023-02-10 15:11:43 -0300

    Adição da semana 2

---
+++ Semana2/Aula1/functions.js
@@ -0,0 +1,27 @@
+/* onsole.log("Hello");
+
+const alunos = ['Gabriel', 'Tales', 'Douglas']
+const limite = alunos.length
+for(i=0; i < limite; i++ ) {
+    console.log(alunos[i]);
+}
+*/
+
+/*const alunos = ['Gabriel', 'Tales', 'Douglas']
+alunos.forEach( ( aluno ) => { 
+    console.log(aluno)
+})*/
+
+/*const numeros = [1, 2, 3, 4, 5, 6, 7]
+numeros.forEach( (numero) => {
+    console.log(numero * 2)
+})*/
+
+/*const numeros = [1, 2, 3, 4, 5, 6, 7]
+let somaNumeros = 0
+numeros.forEach( (numero) => {
+    somaNumeros = somaNumeros + numero
+})
+const media = somaNumeros/numeros.length
+console.log(media);
+*/
