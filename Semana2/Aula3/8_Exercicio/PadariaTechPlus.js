7edda7e0e883440db7f9c8503a9b6b57a0ec8b0a
Edilene <seu@email.comedinunnes15@gmail.com>
2023-02-20 08:56:33 -0300

    Adição Exercício 8

--- Semana2/Aula3/8_Exercicio/PadariaTechPlus.js
+++ Semana2/Aula3/8_Exercicio/PadariaTechPlus.js
@@ -16,21 +16,36 @@ Total: R$ 9.00
 Dinheiro: R$ 20.00
 Troco: R$ 11.00*/
 let listaValoresProdutos = []
-let totaDosProdutos = 0;
+let totalDosProdutos = 0;
 let totalTroco = 0;
+
 function adicionarValorDoProduto(){
-debugger;
-    //Captura o valor informado no campo input da tela e atribui a variável: valorDoProduto;
     var valorDoProduto = document.getElementById("valorDoProduto").value;
-    
+
+    if(valorDoProduto < 0){
+        alert("O valor do produto deve ser maior que zero.");
+        return;
+    }
+
+    if(valorDoProduto == 0 && listaValoresProdutos.length == 0){
+        alert("Não existe compra a ser encerrada.");
+        document.getElementById("valorDoProduto").value = "";
+        return;
+    }
+
     if( valorDoProduto == 0) {
         listaValoresProdutos.forEach(function(item){
-            totaDosProdutos = (totaDosProdutos + Number(item));
+            totalDosProdutos = (totalDosProdutos + Number(item));
         });
-        document.getElementById("valoTotal").innerHTML = "Valor total da compra R$: "+totaDosProdutos
+        document.getElementById("valoTotal").innerHTML = "Valor total da compra R$: "+totalDosProdutos
         document.getElementById("valorDoProduto").style.display = "none";
-        document.getElementById("botao").style.display = "none";
-        document.getElementById("idLabelProduto").style.display = "none";   
+        document.getElementById("botaoAdicionar").style.display = "none";
+        document.getElementById("idLabelProduto").style.display = "none"; 
+
+        document.getElementById("idLabelDinheiro").style.display = ""; 
+        document.getElementById("valorRecebido").style.display = ""; 
+        document.getElementById("botaoCalcular").style.display = ""; 
+
     }else{
         listaValoresProdutos.push(valorDoProduto);
     }
@@ -38,13 +53,42 @@ debugger;
 }
 
 function valorRecebido(){
-    debugger
     var valorRecebido = document.getElementById("valorRecebido").value;
-    totalTroco = valorRecebido - totaDosProdutos;
-
+    totalTroco = valorRecebido - totalDosProdutos;
     document.getElementById("idDinheiro").innerHTML = "Valor do dinheiro R$: "+valorRecebido;
     document.getElementById("idTroco").innerHTML = "Valor do Troco R$: "+totalTroco;
-    
-    
+
+    document.getElementById("valorRecebido").style.display = "";
+    document.getElementById("idDinheiro").style.display = "";
+    document.getElementById("idTroco").style.display = "";
+    document.getElementById("valoTotal").style.display = "";
+
+    document.getElementById("idLabelDinheiro").style.display = "none";
+    document.getElementById("valorRecebido").style.display = "none";
+    document.getElementById("botaoCalcular").style.display = "none";
+
 }
+
+function novaCompra(){
+    listaValoresProdutos = [];
+    totalDosProdutos = 0;
+    totalTroco = 0; 
+    
+    document.getElementById("idLabelDinheiro").style.display = "none";
+    document.getElementById("valorRecebido").style.display = "none";
+    document.getElementById("botaoCalcular").style.display = "none";
+    document.getElementById("idDinheiro").style.display = "none";
+    document.getElementById("idTroco").style.display = "none";
+    document.getElementById("valoTotal").style.display = "none";
+
+    document.getElementById("idLabelProduto").style.display = "";
+    document.getElementById("valorDoProduto").style.display = "";
+    document.getElementById("botaoAdicionar").style.display = "";
+}
+
+document.getElementById("idLabelDinheiro").style.display = "none";
+document.getElementById("valorRecebido").style.display = "none";
+document.getElementById("botaoCalcular").style.display = "none";
+document.getElementById("idDinheiro").style.display = "none";
+document.getElementById("idTroco").style.display = "none";
 
