a2e27d1fa6ef9a6cb7ab4c2cd66a452270385b2c
Edilene <seu@email.comedinunnes15@gmail.com>
2023-02-20 17:44:17 -0300

    Adição do exercício3

--- Semana2/Aula3/3_Exercicio/loginLStorage.js
+++ Semana2/Aula3/3_Exercicio/loginLStorage.js
@@ -3,3 +3,29 @@ Quando o usuário logar, salvar as credenciais localmente.
 Se as credenciais existirem localmente, ir para uma página com um botão de logout, 
 se elas não existirem, ir para a página de login.*/
 
+function login(){
+    let user = document.getElementById("user");
+    let password = document.getElementById("password");
+    document.getElementById("botaoLogin").style.display = " ";
+
+    let credenciais = {
+        nome: user.value,
+        senha: password.value
+    }
+
+    if(credenciais.nome == 'Clara' && credenciais.senha == '4567'){
+      localStorage.setItem('acesso', true);
+      localStorage.setItem('user', JSON.stringify(credenciais));
+        //trocar de janela
+      window.location = "menu.html";
+    }else{
+        alert("Não foi possível realizar o login!")
+    }
+}
+function logout(){
+    let acesso = false
+    localStorage.setItem("acesso", JSON.stringify(acesso))
+    
+        window.location = "index.html";
+
+}
