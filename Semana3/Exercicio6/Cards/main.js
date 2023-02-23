8f6878b4df5264747274ba32cdd2833ae9572b01
Edilene <seu@email.comedinunnes15@gmail.com>
2023-02-23 16:09:04 -0300

    Exercicio em andamento

---
+++ Semana3/Exercicio6/Cards/main.js
@@ -0,0 +1,35 @@
+/*Utilize a API abaixo para buscar um json e criar um cartão com as informações de um usuário do Ricky and Morty.
+Onde você vê o número um, pode mudar para qualquer número até 800 que vai buscar um personagem diferente.
+https://rickandmortyapi.com/api/character/1*/
+
+let idPesquisaPersonagem = 1
+
+function card(){
+
+  idPesquisaPersonagem = Number(document.getElementById("IdPesquisar").value);
+
+  searce()
+};
+
+async function searce() {
+  const response = await fetch(`https://rickandmortyapi.com/api/character/${idPesquisaPersonagem}`)
+  const data = await response.json()
+  console.log(data)
+
+  document.getElementById('name').innerHTML = data?.name;
+  document.getElementById('status').innerHTML = data?.status;
+  document.getElementById('species').innerHTML = data?.species;
+  document.getElementById("avatar").src = data.image;
+  document.getElementById('type').innerHTML = data?.type;
+
+  document.getElementById('origin').innerHTML = data?.origin.name;
+
+
+  if(data?.Male != undefined){
+    document.getElementById('Male').innerHTML = data?.Male  
+  }
+  
+};
+searce()
+
+
