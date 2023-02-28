c4e2ddf7ab81ddf066b12a62f4e86d5214299ec9
Edilene <seu@email.comedinunnes15@gmail.com>
2023-02-28 13:06:46 -0300

    Adição do exercicio 4

---
+++ Semana3/Exercicio4/alarmeNiver/main.js
@@ -0,0 +1,24 @@
+/*Use uma sintaxe que chama uma função em intervalos de tempo definidos e faça ela verificar se chegou uma data, 
+se essa data chegou, ela deve imprimir feliz aniversário. Se não chegou, ela deve dizer quantos dias faltam para chegar a data.
+Para facilitar o cálculo de dias, pode se basear no algorítimo abaixo.
+const d1 = '2021-10-05';
+const d2 = '2021-11-12';
+const diffInMs = new Date(d2) - new Date(d1)
+const diffInDays = diffInMs / (1000 60 60 * 24);
+console.log(diffInDays) // 38*/
+
+setInterval(() =>{
+
+  //const d1 = '2021-10-05';
+  const d2 = '2023-02-28';
+  const diffInMs = new Date(d2) - new Date( )
+  const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
+  
+  if (diffInMs>0) {
+      console.log(`Faltam ${diffInDays} dias`)
+  }else{
+      console.log(`Feliz aniversário`)
+  }
+  
+  
+  }, 3000)
